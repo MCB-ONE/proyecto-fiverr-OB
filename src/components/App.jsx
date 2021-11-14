76474aa460981b2b6723db520ae858206681c5c7
toni <toni.one.dev@gmail.com>
2021-11-14 21:18:43 +0100

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Función Anónima para crear un Componente principal
+ * @returns {React.Component} Componente principal de nuestra aplicación
+ */
+const App = () => {
+    return (
+      <div>
+        <h1>Proyecto final</h1>
+      </div>
+    );
+};
+
+export default App;
